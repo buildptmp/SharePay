12f50b880cbe94f426218eb3b2f1eb24ac24279f
Nichawee Treeprateepsilp <nichawee.tre@student.mahidol.ac.th>
2023-01-22 16:57:24 +0700

    feat: add entry page

---
+++ src/page/UserRegister.js
@@ -0,0 +1,45 @@
+import React, { FC, ReactElement, useState } from "react";
+import { Button, StyleSheet, TextInput, TouchableOpacity, Text, View } from "react-native";
+import { Styles } from "../Styles"
+//import Parse from "parse/react-native";
+
+export default function UserRegister() {
+    const [username, setUsername] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [phonenum, setPhoneNum] = useState(null);
+
+    return(
+
+        <>
+        <TextInput
+            style={Styles.input}
+            value={username}
+            placeholder={"Insert Username"}
+            onChangeText={(text) => setUsername(text)}
+            autoCapitalize={"none"}
+        />
+
+        <TextInput
+            style={Styles.input}
+            value={password}
+            placeholder={"Insert Password"}
+            secureTextEntry
+            onChangeText={(text) => setPassword(text)}
+        />
+
+        <TextInput
+            style={Styles.input}
+            value={phonenum}
+            placeholder={"Insert Phone Number"}
+            onChangeText={(text) => setPhoneNum(text)}
+        />
+        <View style={[{ width: '100%', paddingHorizontal: 100, flex: 3}]}>
+        <TouchableOpacity
+            style={Styles.btn}
+        >
+            <Text style={Styles.text}>Request OTP</Text>
+        </TouchableOpacity>
+        </View> 
+        </>
+    );
+};
