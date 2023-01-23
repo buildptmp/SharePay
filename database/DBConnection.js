4056e988a76fdcf1839ed067ffff1f5a5f87fb29
PS-pop <pathty.s10@gmail.com>
2023-01-23 15:42:04 +0700

    add DBConnection

---
+++ database/DBConnection.js
@@ -0,0 +1,57 @@
+import { initializeApp } from "firebase/app";
+// import { getAuth } from 'firebase/auth';
+import { 
+  getFirestore, collection, getDocs, initializeFirestore, addDoc
+} from "firebase/firestore"
+const firebaseConfig = {
+  apiKey: "AIzaSyDGeiVvbQ_zNcXpbrXsGheivJSE5xAqrt0",
+  authDomain: "sharepay-77c6c.firebaseapp.com",
+  databaseURL: "https://sharepay-77c6c.firebaseio.com",
+  projectId: "sharepay-77c6c",
+  storageBucket: "sharepay-77c6c.appspot.com",
+  messagingSenderId: "G-9ME2LJGL9Q",
+  appId: "1:927888254427:android:bc2b3ddb87e075072e33fe"
+};
+
+const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+// const db = getFirestore();
+const db = initializeFirestore(app, {
+    experimentalForceLongPolling: true,
+});
+const colRefUser = collection(db, 'User')
+
+export function getfromUsers(){
+  getDocs(colRefUser)
+  .then((snapshot) => {
+    let users = []
+    snapshot.docs.forEach((doc) => {
+      users.push({ ...doc.data(), id: doc.id })
+    })
+    console.log(users)
+  })
+  .catch(err => {
+    console.log(err.message)
+  })
+}
+
+export function addtoUsers(){
+  const data = {
+    Password: 'demo2',
+    Phonenumber: '800',
+    UserID: 3,
+    User_image: 'URL',
+    Username: 'demo2'
+  };
+  
+  addDoc(colRefUser, data)
+    .then(docRef => { //create docRef
+        console.log(dbRef.id);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+
+}
+  
+export default colRefUser
