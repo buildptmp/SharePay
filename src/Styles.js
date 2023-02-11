bc8da3d096f0c581c915987889d796b5a75972d8
PS-pop <pathty.s10@gmail.com>
2023-02-11 18:05:04 +0700

    add icon package and update style and DBConnection

--- src/Styles.js
+++ src/Styles.js
@@ -3,16 +3,36 @@ import { StyleSheet } from "react-native";
 export const Styles = StyleSheet.create({
     container: {
         flex:1, 
+        paddingTop: 30, 
         flexDirection: 'column', 
-        justifyContent: 'center', 
+        // justifyContent: 'center', 
+        alignItems: 'center',
+        backgroundColor: '#F6EFEF'
+    },
+
+    containeraddex: {
+        flex:1,
+        paddingTop: 30, 
+        flexDirection: 'column', 
+        // justifyContent: 'center', 
+        alignItems: 'center',
+        backgroundColor: '#F6EFEF'
+    },
+
+    containerginfo: {
+        flex:1,
+        paddingTop: 30, 
+        flexDirection: 'column', 
+        // justifyContent: 'center', 
         alignItems: 'center',
         backgroundColor: '#F6EFEF'
     },
 
     containerAddmem: {
         flex: 2, 
+        paddingTop: 30, 
         flexDirection: 'column', 
-        justifyContent: 'center', 
+        // justifyContent: 'center', 
         alignItems: 'center',
         backgroundColor: '#F6EFEF'
     },
@@ -55,9 +75,19 @@ export const Styles = StyleSheet.create({
 
     inputAddmem: {
         height: 40,
-        width: '90%',
-        marginBottom: 10,
+        width: '100%',
         marginLeft: 10,
+        borderWidth: 1,
+        borderRadius: 15,
+        borderColor: '#B6B6B6',
+        backgroundColor: '#FFFFFF',
+
+    },
+
+    inputaddex: {
+        height: 40,
+        width: '100%',
+        marginBottom: 10,
         borderWidth: 1,
         borderRadius: 15,
         borderColor: '#B6B6B6',
@@ -70,12 +100,35 @@ export const Styles = StyleSheet.create({
         marginBottom: 10,
         padding: 10,
         borderRadius: 20,
+    }, 
+    
+    btnaddex: {
+        backgroundColor: '#F88C8C',
+        marginBottom: 10,
+        padding: 10,
+        borderRadius: 20,
+        marginTop: 20,
+    },
+
+    btngif: {
+        backgroundColor: '#F88C8C',
+        marginBottom: 10,
+        padding: 10,
+        borderRadius: 20,
+        marginTop: 20,
+    },
+
+     btnslip: {
+        backgroundColor: '#F88C8C',
+        marginTop: 20,
+        padding: 10,
+        borderRadius: 20,
     },
 
     btnph: {
         backgroundColor: '#F88C8C',
         width: '50%',
-        marginTop: 20,
+        marginTop: 10,
         padding: 10,
         borderRadius: 20,
         marginLeft:60 ,
@@ -110,5 +163,45 @@ export const Styles = StyleSheet.create({
         borderColor: '#B6B6B6',
         backgroundColor: '#FFFFFF',
     },
+    
+    list_container: {
+        // flex: 1,
+        paddingTop: 5,
+        backgroundColor: '#F6EFEF',
+      },
+    sectionHeader: {
+        paddingTop: 2,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 10,
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    sectionHeaderwithsub: {
+        paddingTop: 2,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 5,
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+    },
 
+    // drop down style
+    dropdownBtnStyle: {
+        width: '80%',
+        height: 50,
+        backgroundColor: '#FFF',
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: '#444',
+        },
+    dropdownBtnTxtStyle: {color: '#444', textAlign: 'left'},
+    dropdownDropdownStyle: {backgroundColor: '#EFEFEF'},
+    dropdownRowStyle: {backgroundColor: '#EFEFEF', borderBottomColor: '#C5C5C5'},
+    dropdownRowTxtStyle: {color: '#444', textAlign: 'left'},
 });
