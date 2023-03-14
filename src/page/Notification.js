b65b1a72a84e05c4e4096bc1b88b85903f3569dd
buildptmp <build_4042@hotmail.com>
2023-03-14 14:52:21 +0700

    notification update

--- src/page/Notification.js
+++ src/page/Notification.js
@@ -12,36 +12,101 @@ import { Button,
     Image,
     TouchableOpacity,
  } from "react-native";
+ 
+ const Noti =() => {
+    const data = [
+        {
+            ID: '01',
+            Username: 'Bilkin',
+            NotiType: ' still have debt in "xxxx" Group',
+            Time: '12.00',
+        },
+        {
+            ID: '02',
+            Username: 'Jino',
+            NotiType: 'User A has invited you to join "XXXX" Group',
+            Time: '16.40',
+        },
+        {
+            ID: '03',
+            Username: 'Capybara',
+            NotiType: 'User A has paid the debt to you',
+            Time: '12.00',
+        },
+        {
+            ID: '04',
+            Username: 'PP',
+            NotiType: 'All expense in "XXXX" group are paid',
+            Time: '12.00',
+        },
+        {
+            ID: '05',
+            Username: 'Kim',
+            NotiType: 'Still have the debt',
+            Time: '12.00',
+        },
+    ]
+ }
 
  export default function Notification({ navigation }) {
-    const [GroupName, setGroupName] = useState(null);
-    const [GroupDesc, setGroupDesc] = useState(null);
-    const RouteMapping = [
-        { routeName: 'AddingMember', displayText: 'Add Member', }
-    ]
-
+    // const [GroupName, setGroupName] = useState(null);
+    // const [GroupDesc, setGroupDesc] = useState(null);
+    // const RouteMapping = [
+    //     { routeName: 'AddingMember', displayText: 'Add Member', }
+    // ];
+    
+        const data = [
+            {
+                ID: '01',
+                Username: 'Bilkin',
+                NotiType: ' still have debt in "xxxx" Group',
+                Time: '12.00',
+            },
+            {
+                ID: '02',
+                Username: 'Jino',
+                NotiType: 'User A has invited you to join "XXXX" Group',
+                Time: '16.40',
+            },
+            {
+                ID: '03',
+                Username: 'Capybara',
+                NotiType: 'User A has paid the debt to you',
+                Time: '12.00',
+            },
+            {
+                ID: '04',
+                Username: 'PP',
+                NotiType: 'All expense in "XXXX" group are paid',
+                Time: '12.00',
+            },
+            {
+                ID: '05',
+                Username: 'Kim',
+                NotiType: 'Still have the debt',
+                Time: '12.00',
+            },
+        ]
+     
     return(
-        
-        <View style={Styles.container}>
-            <View style={[{flex:1}]} />
-            <Image 
-                style = {Styles.logoImg}
-                source={require('../assets/AddMem.png')} 
-            />
-        
+        <View>
+            <FlatList
+                data={data}
+                keyExtractor={(item, index)=> {
+                    return index.toString();
+                }}
+                renderItem={({item})=>{
+                    return (
+                        <View style={Styles.list_container}>
+                            <View>
+                                <Text> {item.Username} </Text>
+                            </View>
+            
+                        </View>
+                    )
+                }}>
+            </FlatList>
+        </View>
        
-            <View style={[{ width: '100%', paddingHorizontal: 100, flex: 3, backgroundColor: '#F6EFEF'}]}>
-                <Text style={Styles.textboxtop}>Group: (wait for data)</Text>
-                <Text style={Styles.textboxtop}>From: 'Debtor'</Text>
-                <Text style={Styles.textboxtop}>To: 'Creditor' </Text>
-                <Text style={Styles.textboxtop}> Amount: 'Price' </Text>
-                <TouchableOpacity 
-                    style={Styles.btnslip}
-                    onPress={()=> alert('Upload Successfully.')}
-                >
-                    <Text style={Styles.text}> Confirm </Text>
-                </TouchableOpacity>
-            </View>
-        </View> 
     );
 };
