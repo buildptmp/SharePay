dec81b50f7306e671d8d8d09d52593bf3823f5db
PS-pop <pathty.s10@gmail.com>
2023-03-15 22:03:06 +0700

    add expenseDetail and remove warning unique key

--- src/page/Debtlist.js
+++ src/page/Debtlist.js
@@ -19,22 +19,19 @@ export default function DebtView({page, navigation}){
         if(listof.havedata){
             setDebtorList(listof.debtor);
             setDebtList(listof.debt);
-        }
-        //console.log('Debtor: ', listof.debtor[0].data)
-        //console.log('Debt: ', list[1][0].data)
-        if (listof.havedata) {
             setLoading(false);
         } else {
             setLoading(true);
         }
+        
+        //console.log('Debtor: ', listof.debtor[0].data)
+        //console.log('Debt: ', listof.debt[0].data)
     }
-    //console.log(debtorList[0].data)
 
     useEffect(() => {
         const uid = auth().currentUser.uid;
         if (!uid) return;
         _showDebtAndDebtorList(uid)
-
     }, [auth().currentUser.toString, isDebtAcitve, isDebtorAcitve, isLoading])
 
     return(
@@ -68,17 +65,6 @@ export default function DebtView({page, navigation}){
             {
                 (isDebtorAcitve && !isLoading) && <DebtorList data={debtorList} />
             }
-
-            {/* // <DebtList
-            // values={['Debt list', 'Debtor list']}
-            // // selectedValue={}
-            // // setSelectedValue={}
-            // >
-            //     <View>
-            //         <SectionList></SectionList>
-            //     </View>
-
-            // </DebtList> */}
         </SafeAreaView>
     );
 };
@@ -93,16 +79,16 @@ function DebtList({data, page}) {
         <SafeAreaView>
             {data.map((e, index) => {
                 return (
-                    <>
-                        <Text style={{fontWeight: 'bold', marginLeft: 10, marginRight: 10,fontSize:18, marginBottom:5,}} key={index}>{e.title}</Text>
-                        { e.data && e.data.map((r) => {
+                    <React.Fragment key={index}>
+                        <Text style={{fontWeight: 'bold', marginLeft: 10, marginRight: 10,fontSize:18, marginBottom:5,}} key={e+index}>{e.title}</Text>
+                        { e.data && e.data.map((r,index) => {
                             return (
-                                <View style={Styles.box}>
+                                <TouchableOpacity style={Styles.box} key={r+index} onPress={()=>{navigation.navigate('Detail',{detail: r.detail, DebtorDebtor: "Debt", gname:e.title, DebtorDebtorName:r.creditorName})}}>
                                 <Text key={r.creditorName} style={Styles.debttext1}>{r.creditorName}</Text>
                                 <Text key={r.debtStatus} style={Styles.debttext2}>{r.debtStatus}</Text>
                                 <Text key={r.calPrice}>{r.calPrice}</Text>
                                 <Text key={r.totolPrice} style={Styles.debttext3}>{r.totolPrice}</Text> 
-                                {RouteMapping.map((g) => {
+                                {RouteMapping.map((g, index) => {
                                     return(
                                 <TouchableOpacity 
                                     key={g.routeName}
@@ -113,10 +99,10 @@ function DebtList({data, page}) {
                                 </TouchableOpacity>
                                 )
                                 })}
-                                </View>
+                                </TouchableOpacity>
                             )
                         })}
-                    </>
+                    </React.Fragment>
                 )
             })}
         </SafeAreaView>
@@ -124,23 +110,25 @@ function DebtList({data, page}) {
 }
 
 function DebtorList({data}) {
+    const navigation = useNavigation();
+
     return (
         <SafeAreaView>
             {data.map((e, index) => {
                 return (
-                    <>
-                        <Text style={{fontWeight: 'bold', marginLeft: 10, marginRight: 10, fontSize:18, marginBottom:5,}} key={index}>{e.title}</Text>
-                        { e.data && e.data.map((t) => {
+                    <React.Fragment key={index}>
+                        <Text style={{fontWeight: 'bold', marginLeft: 10, marginRight: 10, fontSize:18, marginBottom:5,}}>{e.title}</Text>
+                        { e.data && e.data.map((t,index) => {
                             return (
-                                <View style={[Styles.box, {width:'100%'}]}>
+                                <TouchableOpacity style={Styles.box} key={t+index} onPress={()=>{navigation.navigate('Detail',{detail: t.detail, DebtorDebtor: "Debtor", gname:e.title, DebtorDebtorName:t.debtorName})}}>
                                 <Text key={t.debtorName} style={Styles.debttext1}>{t.debtorName}</Text>
                                 <Text key={t.debtStatus} style={[Styles.debttext2, {textAlign:'center'}]}>{t.debtStatus}</Text>
                                 <Text key={t.totolPrice} style={[Styles.debttext3,{width:'30%', textAlign:'right'}]}>{t.totolPrice}</Text>
-                                </View>
+                                </TouchableOpacity>
                             )
                         })
                         }
-                    </>
+                    </React.Fragment>
                 )
             })}
         </SafeAreaView>
--- src/page/ExpenseDetail.js
+++ src/page/ExpenseDetail.js
@@ -1,16 +1,38 @@
 import React, { useState, useEffect } from "react";
-import { TextInput, TouchableOpacity, Text, View, Image } from "react-native";
+import { TextInput, TouchableOpacity, Text, View, Image, SafeAreaView, SectionList } from "react-native";
 import { Styles } from "../Styles"
 
-export default function ExpenseDetail({ page, navigation}) {
-    <View style={[{ width: '100%', paddingHorizontal: 100, flex: 3, backgroundColor: '#F6EFEF'}]}>
-    <TouchableOpacity 
-        // key={e.routeName}
-        style={Styles.btndetail}
-        //onPress={()=> }
-        >
-        <Text style={Styles.text}> Edit </Text>
-    </TouchableOpacity>
-    </View>
+export default function ExpenseDetail({ page, navigation, route}) {
+    const { detail, DebtorDebtor, gname, DebtorDebtorName} = route.params;
+
+
+
+    return(
+        <SafeAreaView style={Styles.list_container}>
+            {detail && <SectionList
+                style={{height:'100%'}}
+                sections={[
+                    {title: DebtorDebtor+" ("+DebtorDebtorName+")" , data: detail},
+                ]}
+                renderItem={({item}) => 
+                    <TouchableOpacity style ={{flex: 1}} onPress={() => 
+                        navigation.navigate('Item Information',{eid:item.eid, allowToEdit:false, gid:item.gid,gname:gname})
+                        }>
+                        <View style={Styles.box}>
+                            {/* <Image style={{borderRadius: 50, height:35, width:35,margin:5 }} source={{uri:item.image}}/> */}
+                            
+                            <Text style={Styles.debttext1}>{item.itemName}</Text>
+                            <Text style={[Styles.debttext2, {textAlign:'center'}]}>{item.debtStatus}</Text>
+                            <Text style={[Styles.debttext3,{width:'30%', textAlign:'right'}]}>{item.priceToPay}</Text>
+                        </View>
+                    </TouchableOpacity>
+                }
+                keyExtractor={(item, index) => item + index}
+                renderSectionHeader={({section}) => (
+                    <Text style={Styles.sectionHeader}>{section.title}</Text>
+                )}
+            />}
+        </SafeAreaView>
+    )
 }
 
